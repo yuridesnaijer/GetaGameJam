1b23fe1c69632d715d43807afae33167b73cb563
yuri <yuri.desnaijer@incentro.com>
2020-01-04 01:48:48 +0100

    Improve Grid

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -5,34 +5,42 @@ using UnityEngine;
 public class GridManager : MonoBehaviour
 {
 
-    public GameObject Cell;
-    public Camera mainCamera;
-    private List<Vector3> gridPositions = new List<Vector3>();
-    // Start is called before the first frame update
+    public GameObject Grid;
+    private GameObject CurrentGrid;
+    private float animationTimer = 0f;
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
     void Start()
     {
-        GenerateGrid(5);
-    }
-
-    private void GenerateGrid(int length)
-    {
-        gridPositions.Clear();
-        for (int x = 0; x < length; x++)
-        {
-            for (int z = 0; z < length; z++)
-            {
-                gridPositions.Add(new Vector3(x, 0, z));
-            }
-        }
-
-        foreach (Vector3 position in gridPositions)
-        {
-            Instantiate(Cell, position, new Quaternion(), this.transform);
-        }
+        CurrentGrid = Instantiate(Grid, new Vector3(), new Quaternion());
     }
 
     public void NextLevelAnimation()
     {
-        this.transform.localScale = new Vector3(.1f, .1f, .1f);
+        GameObject newGrid = Instantiate(Grid, new Vector3(), new Quaternion());
+        newGrid.transform.localScale = new Vector3(5f, 5f, 5f);
+
+        Destroy(CurrentGrid);
+        StartCoroutine(ScaleDownAnimation(3f, newGrid));
+    }
+
+    IEnumerator ScaleDownAnimation(float time, GameObject grid)
+    {
+
+        CurrentGrid = grid;
+
+        float i = 0;
+        float rate = 1 / time;
+        Vector3 fromScale = grid.transform.localScale;
+        Vector3 toScale = Vector3.one;
+        while (i < 1)
+        {
+            i += Time.deltaTime * rate;
+            grid.transform.localScale = Vector3.Lerp(fromScale, toScale, i);
+            yield return 0;
+        }
     }
 }
