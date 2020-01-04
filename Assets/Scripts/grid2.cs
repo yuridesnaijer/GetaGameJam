e3a123bce9b9db658c44956cf6780a405f70a00d
yuri <yuridesnaijer@gmail.com>
2020-01-04 02:20:30 +0100

    update

---
+++ Assets/Scripts/grid2.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class grid2 : MonoBehaviour
+{
+
+    public GameObject Cell;
+    int[] array = new int[5];
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int total = 5;
+
+        float[] row = Enumerable.Range(0,total).Select(i => (float)i/total).ToArray();
+
+        float size = 10;
+
+        var result = 
+            (from x in row
+            from y in row
+            select new Vector3(x * size, 0, y * size)).ToList();
+
+        foreach (Vector3 vec in result)
+        {
+            Instantiate(Cell, vec, new Quaternion(), this.transform);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
